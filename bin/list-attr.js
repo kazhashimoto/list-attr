36d037ff58497b7ec7819395cad6baeaa2facf1a
kazhashimoto <zkar.h62@gmail.com>
2021-10-08 16:40:06 +0900

    Fix version number

--- bin/list-attr.js
+++ bin/list-attr.js
@@ -7,7 +7,7 @@ const debug = require('debug')('list-attr');
 
 program
   .name('list-attr')
-  .version('1.0.0')
+  .version('1.0.3')
   .usage('[options] htmlfile')
   .showHelpAfterError()
   .option('-t <tag>', 'specify tag name (default: img)')
